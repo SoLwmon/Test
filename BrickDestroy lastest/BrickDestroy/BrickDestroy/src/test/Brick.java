0aa52f7464d9bfe337d6c6f697a4f1bd4480ccf4
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-12-09 11:48:19 +0800

    Update Brick.java

--- BrickDestroy lastest/BrickDestroy/BrickDestroy/src/test/Brick.java
+++ BrickDestroy lastest/BrickDestroy/BrickDestroy/src/test/Brick.java
@@ -1,132 +1,296 @@
 package test;
 
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.GeneralPath;
+import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.util.Random;
 
-/**
- * Created by filippo on 04/09/16.
- *
- */
 abstract public class Brick  {
 
-    public static final int MIN_CRACK = 1;
-    public static final int DEF_CRACK_DEPTH = 1;
-    public static final int DEF_STEPS = 35;
+    private static int MIN_CRACK;
+    private static int DEF_CRACK_DEPTH;
+    private static int DEF_STEPS;
 
 
-    public static final int UP_IMPACT = 100;
-    public static final int DOWN_IMPACT = 200;
-    public static final int LEFT_IMPACT = 300;
-    public static final int RIGHT_IMPACT = 400;
-
-    public boolean containsPowerUp = false;
+    private static int UP_IMPACT;
+    private static int DOWN_IMPACT;
+    private static int LEFT_IMPACT;
+    private static int RIGHT_IMPACT;
 
 
     private static Random rnd;
 
     private String name;
-    Shape brickFace;
+    private Shape brickFace;
 
     private Color border;
     private Color inner;
 
     private int fullStrength;
-    int strength;
+    private int strength;
 
     private boolean broken;
 
-    public int x;
-    public int y;
-    public int w;
-    public int h;
-    public int line;
-    public int indexLine;
-    public boolean collisionable = true;
+    /**
+     * Brick Constructor to be called when new brick needs to be designed
+     * @param name name of the brick
+     * @param pos coordinate X and Y of brick
+     * @param size size of the brick
+     * @param border color of the brick border
+     * @param inner color of the brick for the inner part
+     * @param strength strength of the brick
+     */
     public Brick(String name, Point pos,Dimension size,Color border,Color inner,int strength){
-        rnd = new Random();
+        setMinCrack(1);
+        setDefCrackDepth(1);
+        setDefSteps(35);
+        setUpImpact(100);
+        setDownImpact(200);
+        setLeftImpact(300);
+        setRightImpact(400);
+
+        setRnd(new Random());
         broken = false;
         this.name = name;
-        brickFace = makeBrickFace(pos,size);
+        setBrickFace(makeBrickFace(pos,size));
         this.border = border;
         this.inner = inner;
-        this.fullStrength = this.strength = strength;
-        x=pos.x;
-        y=pos.y;
-        w=size.width;
-        h=size.height;
+        this.setStrength(strength);
+        this.fullStrength = getStrength();
+
     }
 
+    /**
+     * abstract method to make the type of brick
+     * @param pos Coordinate X and Y of brick
+     * @param size size of the brick
+     * @return new brick that has the shape of a rectangle
+     */
     protected abstract Shape makeBrickFace(Point pos,Dimension size);
 
-    public  boolean setImpact(Point2D point , int dir){
+    /**
+     * When the brick has an impact, this method will be used
+     * @param point Coordinate of X and Y of the brick
+     * @param dir direction of brick that is impacted
+     * @return the brick is broken
+     */
+    public boolean setImpact(Point2D point , int dir){
         if(broken)
             return false;
         impact();
-        return  broken;
+        return broken;
     }
 
+    /**
+     * abstract method to get the type of brick
+     * @return the type of brick
+     */
     public abstract Shape getBrick();
 
-    public Shape getPowerUpEmplacement() {
-        return new Ellipse2D.Float(x+w/2-h/4,y+h/4,h/2,h/2);
-    }
-
-    public Shape getPowerUpEmplacement2() {
-        return new Ellipse2D.Float(x+w/2-h/4+3,y+h/4+3,h/2-6,h/2-6);
-    }
-
-    public Shape getPowerUpEmplacement3() {
-        return new Ellipse2D.Float(x+w/2-h/4+5,y+h/4+5,h/2-10,h/2-10);
-    }
-
-    public Color getBorderColor(){
-        return  border;
-    }
-
-    public Color getInnerColor(){
-        return inner;
-    }
-
-
+    /**
+     * what happens when the ball and brick collided
+     * @param b the type of ball
+     * @return an integer to make the amount of force that the ball and brick hits
+     */
     public final int findImpact(Ball b){
         if(broken)
             return 0;
         int out  = 0;
-        if(brickFace.contains(b.right))
-            out = LEFT_IMPACT;
-        else if(brickFace.contains(b.left))
-            out = RIGHT_IMPACT;
-        else if(brickFace.contains(b.up))
-            out = DOWN_IMPACT;
-        else if(brickFace.contains(b.down))
-            out = UP_IMPACT;
+        if(getBrickFace().contains(b.getRight()))
+            out = getLeftImpact();
+        else if(getBrickFace().contains(b.getLeft()))
+            out = getRightImpact();
+        else if(getBrickFace().contains(b.getUp()))
+            out = getDownImpact();
+        else if(getBrickFace().contains(b.getDown()))
+            out = getUpImpact();
         return out;
     }
 
+    /**
+     * make the brick broken
+     * @return the brick is broken
+     */
     public final boolean isBroken(){
         return broken;
     }
 
+    /**
+     * reset the brick, make it not broken and has the full strength of its brick
+     */
     public void repair() {
         broken = false;
-        strength = fullStrength;
+        setStrength(fullStrength);
     }
 
+    /**
+     * when the brick has impacted, reduce the strength of it, if the strength is 0 then make the brick is broken
+     */
     public void impact(){
-        strength--;
-        broken = (strength == 0);
+        setStrength(getStrength() - 1);
+        broken = (getStrength() == 0);
     }
 
-    public void setPowerUp() {
-        this.containsPowerUp=true;
+    /**
+     * get method for Up_Impact, encapsulating
+     * @return Up Impact of brick
+     */
+    public static int getUpImpact() {
+        return UP_IMPACT;
     }
 
+    /**
+     * set method for Up_Impact, encapsulating
+     * @param upImpact Up_Impact integer
+     */
+    public static void setUpImpact(int upImpact) {
+        UP_IMPACT = upImpact;
+    }
+
+    /**
+     * get method for Down_Impact, encapsulating
+     * @return Down Impact of brick
+     */
+    public static int getDownImpact() {
+        return DOWN_IMPACT;
+    }
+
+    /**
+     * set method for Down_Impact, encapsulating
+     * @param downImpact Down_Impact integer
+     */
+    public static void setDownImpact(int downImpact) {
+        DOWN_IMPACT = downImpact;
+    }
+
+    /**
+     * get method for Left_Impact, encapsulating
+     * @return Left Impact of brick
+     */
+    public static int getLeftImpact() {
+        return LEFT_IMPACT;
+    }
+
+    /**
+     * set method for Left_Impact, encapsulating
+     * @param leftImpact Left_Impact integer
+     */
+    public static void setLeftImpact(int leftImpact) {
+        LEFT_IMPACT = leftImpact;
+    }
+
+    /**
+     * get method for Right_Impact, encapsulating
+     * @return Right Impact of brick
+     */
+    public static int getRightImpact() {
+        return RIGHT_IMPACT;
+    }
+
+    /**
+     * set method for Right_Impact, encapsulating
+     * @param rightImpact Right_Impact integer
+     */
+    public static void setRightImpact(int rightImpact) {
+        RIGHT_IMPACT = rightImpact;
+    }
+
+    /**
+     * get method for Border Color, encapsulating
+     * @return the border colour of brick
+     */
+    public Color getBorderColor(){
+        return  border;
+    }
+
+    /**
+     * get method for Inner Color, encapsulating
+     * @return the inner colour of the brick
+     */
+    public Color getInnerColor(){
+        return inner;
+    }
+
+    /**
+     * get method for Brick Face, encapsulating
+     * @return the face of the brick, like its color
+     */
+    public Shape getBrickFace() {
+        return brickFace;
+    }
+
+    /**
+     * set method for Brick Face, encapsulating
+     * @param brickFace face of the brick
+     */
+    public void setBrickFace(Shape brickFace) {
+        this.brickFace = brickFace;
+    }
+
+    /**
+     * get method for random, encapsulating
+     * @return random number
+     */
+    public static Random getRnd() {
+        return rnd;
+    }
+
+    /**
+     * set method for random, encapsulating
+     * @param rnd random number for steel brick
+     */
+    public static void setRnd(Random rnd) {
+        Brick.rnd = rnd;
+    }
+
+    /**
+     * get method for Def_Crack_Depth, encapsulating
+     * @return the crack depth of the brick
+     */
+    public static int getDefCrackDepth() {
+        return DEF_CRACK_DEPTH;
+    }
+
+    /**
+     * get method for Def_Steps, encapsulating
+     * @return the steps of the brick
+     */
+    public static int getDefSteps() {
+        return DEF_STEPS;
+    }
+
+    /**
+     * set method for Min_Crack, encapsulating
+     * @param minCrack minimum crack of the brick
+     */
+    public static void setMinCrack(int minCrack) {
+        MIN_CRACK = minCrack;
+    }
+
+    /**
+     * set method for Def_Crack_Depth, encapsulating
+     * @param defCrackDepth crack depth of the brick
+     */
+    public static void setDefCrackDepth(int defCrackDepth) {
+        DEF_CRACK_DEPTH = defCrackDepth;
+    }
+
+    /**
+     * set method for Def_Steps, encapsulating
+     * @param defSteps steps of the brick
+     */
+    public static void setDefSteps(int defSteps) {
+        DEF_STEPS = defSteps;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
-
-
 
 
 
