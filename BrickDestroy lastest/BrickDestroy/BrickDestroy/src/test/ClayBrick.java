3e4124e64d3dcb745736b46507631c8d8ee6e025
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-12-12 23:26:15 +0800

    Update ClayBrick.java

--- BrickDestroy lastest/BrickDestroy/BrickDestroy/src/test/ClayBrick.java
+++ BrickDestroy lastest/BrickDestroy/BrickDestroy/src/test/ClayBrick.java
@@ -1,9 +1,7 @@
-package test;
+package model;
 
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
-import java.awt.geom.RoundRectangle2D.Float;
-
 
 /**
  * Created by filippo on 04/09/16.
@@ -12,26 +10,38 @@ import java.awt.geom.RoundRectangle2D.Float;
 public class ClayBrick extends Brick {
 	static final Color[] COLOR_INNER =  new Color[] {new Color(0xff0036),new Color(0xfd8317),new Color(0xe3c900), new Color(0x34c61d), new Color(0x4591da)};
 	static final Color[] COLOR_BORDER =  new Color[] {new Color(0xc00036),new Color(0xd86500),new Color(0xc8af00), new Color(0x26a812), new Color(0x2879c9)};
-	
-    private static final String NAME = "Clay Brick";
-    private static final Color DEF_INNER = new Color(178, 34, 34).darker();
-    private static final Color DEF_BORDER = Color.GRAY;
+
     private static final int CLAY_STRENGTH = 1;
 
+    /**
+     * Clay Brick Constructor
+     * @param point
+     * @param size
+     */
     public ClayBrick(Point point, Dimension size){
     	
-        super(NAME,point,size,(int)(point.y/size.height)>4?COLOR_BORDER[0]:COLOR_BORDER[(int)(point.y/size.height)].darker(),(int)(point.y/size.height)>4?COLOR_INNER[0]:COLOR_INNER[(int)(point.y/size.height)],CLAY_STRENGTH);
+        super(point,size, (point.y/size.height) >4?COLOR_BORDER[0]:COLOR_BORDER[point.y/size.height].darker(), (point.y/size.height) >4?COLOR_INNER[0]:COLOR_INNER[point.y/size.height],CLAY_STRENGTH);
 
     }
 
+    /**
+     * Create a clay brick
+     * @param pos coordinate of the brick
+     * @param size size of the brick
+     * @return clay brick structure
+     */
     @Override
     protected Shape makeBrickFace(Point pos, Dimension size) {
         return new RoundRectangle2D.Float(pos.x,pos.y,size.width, size.height, 5,5);
     }
 
+    /**
+     * Get the type of the brick
+     * @return
+     */
     @Override
     public Shape getBrick() {
-        return super.brickFace;
+        return super.getBrickFace();
     }
 
 
