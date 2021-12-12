e79eddbf3c27c4e958d8b8adc6bd72356cc0da60
SoLwmon <83406615+SoLwmon@users.noreply.github.com>
2021-12-12 23:53:47 +0800

    Update RubberBall.java

--- test/RubberBall.java
+++ test/RubberBall.java
@@ -15,7 +15,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package test;
+package model;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
@@ -25,21 +25,52 @@ public class RubberBall extends Ball {
 
 
     private static final int DEF_RADIUS = 10;
-    private static final Color DEF_INNER_COLOR = new Color(255, 219, 88);
-    private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker().darker();
+    private static final Color DEF_INNER_COLOR = new Color(0xa4fffc);
+    private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker();
+    private static final Color DEF_INNER_COLOR_SPEED = new Color(0xff4a3c);
+    private static final Color DEF_BORDER_COLOR_SPEED = new Color(0x9f362e);
 
-
+    /**
+     * Rubberball constructor
+     * @param center center of the ball
+     */
     public RubberBall(Point2D center){
         super(center,DEF_RADIUS,DEF_RADIUS,DEF_INNER_COLOR,DEF_BORDER_COLOR);
     }
 
-
+    /**
+     * To make the ball
+     * @param center center of the ball
+     * @param radiusA the radius of x axis
+     * @param radiusB the radius of y axis
+     * @return the rubber ball
+     */
     @Override
     protected Shape makeBall(Point2D center, int radiusA, int radiusB) {
 
-        double x = center.getX() - (radiusA / 2);
-        double y = center.getY() - (radiusB / 2);
+        double x = center.getX() - (radiusA);
+        double y = center.getY() - (radiusB);
 
         return new Ellipse2D.Double(x,y,radiusA,radiusB);
     }
+
+    /**
+     * TO change the ball type
+     * @param state changes of the ball to fireball
+     */
+	@Override
+	public void changeColorState(int state) {
+        switch (state) {
+            case STATE_NORMAL -> {
+                this.setBorderColor(DEF_BORDER_COLOR);
+                this.setInnerColor(DEF_INNER_COLOR);
+            }
+            case STATE_FIRE -> {
+                this.setBorderColor(DEF_BORDER_COLOR_SPEED);
+                this.setInnerColor(DEF_INNER_COLOR_SPEED);
+            }
+        }
+	}
+    
+    
 }
